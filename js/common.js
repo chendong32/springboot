1c08e66eda01ed344fbe947b13f7e60e3a28e040
chendong32 <434611969@qq.com>
2018-10-13 11:21:56 +0800

    canvas绘制图片前预加载避免出现绘制时未加载完成的情况

--- js/common.js
+++ js/common.js
@@ -3,43 +3,57 @@
  */
 var com = com || {};
 
+com.pointStartX = 7;
+com.pointStartY = 169;
+com.spaceX = 95 + 11;
+com.spaceY = 95 + 12;
+
 function init() {
-    com.canvas = document.getElementById("chess"); //画布
+    com.canvas = document.getElementById("chess");
     com.ct = com.canvas.getContext("2d");
+    com.ct.shadowOffsetX = 2;
+    com.ct.shadowOffsetY = 4;
+    com.ct.shadowBlur = 2;
+    com.ct.shadowColor = "rgba(0,0,0,0.4)";
+    loadImages();
+}
+com.images = ['a_b', 'b_c', 'b_j', 'b_m', 'b_p', 'b_s', 'b_x', 'b_z', 'r_c', 'r_j', 'r_m', 'r_p', 'r_s', 'r_x', 'r_z', 'z_c'];
+function loadImages() {
+    console.log("loadImages");
+    com.abImg = new Image();
+    com.abImg.src = "./img/" + com.images[0] + ".png";
+    for (var i = 1; i < com.images.length - 1; i++) {
+        com[i] = {};
+        com[i].image = new Image();
+        com[i].image.src = "./img/" + com.images[i] + ".png";
+    }
+    com.zcImg = new Image();
+    com.zcImg.src = "./img/" + com.images[com.images.length - 1] + ".png";
+}
 
-    var Map = [
-        ['b_c_0', 'b_m_0', 'b_x_0', 'b_s_0', 'b_j_0', 'b_s_1', 'b_x_1', 'b_m_1', 'b_c_1'],
-        [, , , , , , , ,],
-        [, 'b_p_0', , , , , , 'b_p_1',],
-        ['b_z_0', , 'b_z_1', , 'b_z_2', , 'b_z_3', , 'b_z_4'],
-        [, , , , , , , ,],
-        [, , , , , , , ,],
-        ['r_z_0', , 'r_z_1', , 'r_z_2', , 'r_z_3', , 'r_z_4'],
-        [, 'r_p_0', , , , , , 'r_p_1',],
-        [, , , , , , , ,],
-        ['r_c_0', 'r_m_0', 'r_x_0', 'r_s_0', 'r_j_0', 'r_s_1', 'r_x_1', 'r_m_1', 'r_c_1']
-    ];
-
-    com.ct.shadowOffsetX = 2; // 设置水平位移
-    com.ct.shadowOffsetY = 4; // 设置垂直位移
-    com.ct.shadowBlur = 2; // 设置模糊度
-    com.ct.shadowColor = "rgba(0,0,0,0.4)"; // 设置阴影颜色
-
-    for (var i = 0; i < Map.length; i++) {
-        for (var j = 0; j < Map[i].length; j++) {
-            var img_b = new Image();
-            if (Map[i][j] == undefined)
-                continue;
-            img_b.src = "./img/" + Map[i][j].substr(0, 3) + ".png";
-            com.ct.drawImage(img_b, 7 + (95 + 11) * j, 169 + (95 + 12) * i);
+com.initMap = [
+    ['b_c_1', 'b_m_3', 'b_x_6', 'b_s_5', 'b_j_2', 'b_s_5', 'b_x_6', 'b_m_3', 'b_c_1'],
+    [, , , , , , , ,],
+    [, 'b_p_4', , , , , , 'b_p_4',],
+    ['b_z_7', , 'b_z_7', , 'b_z_7', , 'b_z_7', , 'b_z_7'],
+    [, , , , , , , ,],
+    [, , , , , , , ,],
+    ['r_z_14', , 'r_z_14', , 'r_z_14', , 'r_z_14', , 'r_z_14'],
+    [, 'r_p_11', , , , , , 'r_p_11',],
+    [, , , , , , , ,],
+    ['r_c_8', 'r_m_10', 'r_x_13', 'r_s_12', 'r_j_9', 'r_s_12', 'r_x_13', 'r_m_10', 'r_c_8']
+];
+window.onload = function () {
+    console.log("onload");
+    com.ct.drawImage(com.abImg, 0, 0);
+    for (var i = 0; i < com.initMap.length; i++) {
+        for (var j = 0; j < com.initMap[i].length; j++) {
+            if (com.initMap[i][j]) {
+                var key = com.initMap[i][j].substr(4);
+                com.ct.drawImage(com[key].image, com.pointStartX + com.spaceX * j, com.pointStartY + com.spaceY * i);
+            }
         }
     }
-    var choose = new Image();
-    choose.src = "./img/cr.png";
-    com.ct.drawImage(choose, 7 +(95 + 11) * 4, 169 );
-
-    com.ct.drawImage(choose, 7 +(95 + 11) * 4 , 169 +(95 + 12) * 9);
-
 };
 
 init();
--- js/play.js
+++ js/play.js
@@ -2,3 +2,9 @@
  * Created by Administrator on 2018/10/12.
  */
 var play = play || {};
+
+function init() {
+    console.log("play_init");
+}
+
+init();
